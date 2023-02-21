014290274799b20767e9fac4ba68ad7aa9e02bbf
Juan-45 <quijuan12@gmail.com>
2023-02-21 20:57:29 -0300

    Add sections to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import HomeHeader from "pages/home/HomeHeader";
+import Divider from "components/Divider";
+import Presentation from "pages/home/Presentation";
+import MainServices from "components/MainServices";
 
-const Home = ({ linkUrl }) => {
-  return <HomeHeader linkUrl={linkUrl} />;
+const Home = ({ links }) => {
+  return (
+    <>
+      <HomeHeader linkUrl={links.appointment} />
+      <Divider />
+      <Presentation links={links} />
+      <MainServices links={links} />
+      <Divider />
+    </>
+  );
 };
 
 export default Home;
