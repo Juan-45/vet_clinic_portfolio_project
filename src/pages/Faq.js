8f77644ad5bf23450c10c11673e992947ee97f62
Juan-45 <quijuan12@gmail.com>
2023-02-23 21:00:32 -0300

    Add FAQ page with header

---
+++ src/pages/Faq.js
@@ -0,0 +1,14 @@
+import FaqHeader from "pages/faq/FaqHeader";
+import Divider from "components/Divider";
+import WorkInProgress from "components/TEMPORAL/WorkInProgress";
+
+const Faq = () => (
+  <>
+    <FaqHeader />
+    <Divider />
+    <WorkInProgress />
+    <Divider />
+  </>
+);
+
+export default Faq;
