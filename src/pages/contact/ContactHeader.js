58ab311520ebe2210443126eb0be99f73582cd66
Juan-45 <quijuan12@gmail.com>
2023-02-23 21:00:21 -0300

    Add Contact page with header

---
+++ src/pages/contact/ContactHeader.js
@@ -0,0 +1,27 @@
+import Header from "components/Header";
+import { TextContainer, Blurb } from "components/header/Styles";
+import { HighlightBlue } from "components/CommonStyles";
+import contact_1920x780_c from "assets/contact_1920x780_c.jpg";
+import contact_1200x494_c from "assets/contact_1200x494_c.jpg";
+import contact_900x369_c from "assets/contact_900x369_c.jpg";
+
+const ContactHeader = () => {
+  return (
+    <Header
+      src={contact_1920x780_c}
+      sourceOptions={[
+        { media: "(min-width: 1200px)", srcset: contact_1920x780_c },
+        { media: "(min-width: 900px)", srcset: contact_1200x494_c },
+        { media: "(max-width: 900px)", srcset: contact_900x369_c },
+      ]}
+    >
+      <TextContainer>
+        <Blurb variant='h1' color='primary'>
+          Agenda tu cita <HighlightBlue>ahora</HighlightBlue>.
+        </Blurb>
+      </TextContainer>
+    </Header>
+  );
+};
+
+export default ContactHeader;
