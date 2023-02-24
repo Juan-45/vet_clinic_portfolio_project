1057aa6079d4c2876914d9834d06238324aac4e8
Juan-45 <quijuan12@gmail.com>
2023-02-23 21:00:42 -0300

    Add Ratings page with header

---
+++ src/pages/Ratings.js
@@ -0,0 +1,14 @@
+import RatingsHeader from "pages/ratings/RatingsHeader";
+import Divider from "components/Divider";
+import WorkInProgress from "components/TEMPORAL/WorkInProgress";
+
+const Ratings = () => (
+  <>
+    <RatingsHeader />
+    <Divider />
+    <WorkInProgress />
+    <Divider />
+  </>
+);
+
+export default Ratings;
