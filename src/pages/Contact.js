67747f57e02a79cb0a6af22ca74b316d9628a08d
Juan-45 <quijuan12@gmail.com>
2023-03-06 19:23:12 -0300

    Update Contact page:
    - Add new form
    - Add text

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,35 @@
 import ContactHeader from "pages/contact/ContactHeader";
 import Divider from "components/Divider";
-import WorkInProgress from "components/TEMPORAL/WorkInProgress";
+import { Title, LargeContainer } from "components/CommonStyles";
+import ProminentTextSection from "components/ProminentTextSection";
+import {
+  InnerContainer,
+  ParagraphContainer,
+  Paragraph,
+} from "components/prominentTextSection/Styles";
+import Form from "pages/contact/Form";
 
 const Contact = () => (
   <>
     <ContactHeader />
     <Divider />
-    <WorkInProgress />
+    <LargeContainer className='bottomSpace'>
+      <ProminentTextSection>
+        <Title variant='h1'>¿Tienes preguntas o inquietudes?</Title>
+        <InnerContainer className='oversized secondary'>
+          <ParagraphContainer>
+            <Paragraph>
+              Si tienes alguna duda, pregunta o simplemente deseas comunicarte
+              con nosotros, ¡no dudes en contactarnos! En PetPaw estamos siempre
+              disponibles para atenderte. Ya sea por teléfono, nuestro
+              formulario de contacto o redes sociales, nuestro equipo estará
+              encantado de ayudarte y brindarte la información que necesitas.
+            </Paragraph>
+          </ParagraphContainer>
+        </InnerContainer>
+      </ProminentTextSection>
+    </LargeContainer>
+    <Form />
     <Divider />
   </>
 );
