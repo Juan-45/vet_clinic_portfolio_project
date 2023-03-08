841e8d693ddb30e7ab5568e5188246bfa5f56488
Juan-45 <quijuan12@gmail.com>
2023-03-08 15:57:17 -0300

    Update Faq page

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,12 +1,35 @@
 import FaqHeader from "pages/faq/FaqHeader";
 import Divider from "components/Divider";
-import WorkInProgress from "components/TEMPORAL/WorkInProgress";
+import { Title, LargeContainer } from "components/CommonStyles";
+import ProminentTextSection from "components/ProminentTextSection";
+import {
+  InnerContainer,
+  ParagraphContainer,
+  Paragraph,
+} from "components/prominentTextSection/Styles";
+import FaqSection from "pages/faq/FaqSection";
 
 const Faq = () => (
   <>
     <FaqHeader />
     <Divider />
-    <WorkInProgress />
+    <LargeContainer className='bottomSpace'>
+      <ProminentTextSection>
+        <Title variant='h1'>
+          Resuelve tus dudas con nuestras preguntas frecuentes.
+        </Title>
+        <InnerContainer className='oversized secondary'>
+          <ParagraphContainer>
+            <Paragraph>
+              Las preguntas frecuentes responden a las dudas más comunes que
+              puedan tener nuestros clientes. Consultarlas puede ser de gran
+              ayuda y ahorrar tiempo.
+            </Paragraph>
+          </ParagraphContainer>
+        </InnerContainer>
+      </ProminentTextSection>
+    </LargeContainer>
+    <FaqSection />
     <Divider />
   </>
 );
